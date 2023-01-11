a4e3d082d7088bc12f0ceffed1710ad2005fbe91
garvit <agarvit1142000@gmail.com>
2023-01-11 18:20:01 +0530

    Created middle ware function for validating schema of data in backend

--- src/utils/connect.js
+++ src/utils/connect.js
@@ -11,14 +11,12 @@ async function run(client) {
   try {
     const database = client.db("sample_airbnb");
     const movies = database.collection("listingsAndReviews");
-    // query for a movie that has the title 'Back to the Future'
     const query = { name: "Ribeira Charming Duplex" };
     const movie = await movies.findOne(query);
     console.log(movie);
   } catch (err) {
     console.log(err.stack);
   }
-  // Query for a movie that has the title 'Back to the Future'
 }
 
 const functions = {
