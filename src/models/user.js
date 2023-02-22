a6500caf02394f07c078940f14d9097195f131ff
garvit <agarvit1142000@gmail.com>
2023-02-23 02:21:12 +0530

    Created user schema and presave and compare password function created in that schema

---
+++ src/models/user.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+import dotenv from "dotenv";
+dotenv.config();
+
+const saltWorkFactor = parseInt(process.env.SALT_WORK_FACTOR, 10);
+const userSchema = new mongoose.Schema(
+  {
+    username: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.pre("save", async function (next) {
+  let user = this;
+  if (user.isModified("password")) {
+    // if the password is modified then only we will hash the password before saving it to the database
+    const salt = await bcrypt.genSalt(parseInt(process.env.SALT_WORK_FACTOR));
+
+    user.password = await bcrypt.hash(user.password, salt);
+    return next();
+  } else return next();
+});
+
+const User = mongoose.model("User", userSchema);
+
+// we can crate methods on the schema and then use them on the model like this user.comparePassword
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  let user = this;
+  try {
+    await bcrypt.compare(candidatePassword, user.password);
+    return true;
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+};
+export { User };
+// moongose have hooks  pre and post hooks pre hooks are executed before an event and post hooks are executed after an event pre hooks are used before saving data to database and post hooks are used after saving data to database
