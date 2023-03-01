c7d23d5619ce62cee1845890713fdda8c1be0dd4
garvit <agarvit1142000@gmail.com>
2023-03-01 11:20:52 +0530

    Verify jwt  middleware fot protected route

--- src/middleware/verifyJWT.js
+++ src/middleware/verifyJWT.js
@@ -1,16 +1,23 @@
 import jwt from "jsonwebtoken";
 
 export async function verifyToken(req, res, next) {
-  const token = req.headers.authorization || req.cookies.token;
+  console.log("hello");
+  const token = req.cookies.token || req.headers.authorization;
+  console.log(token);
   if (!token) {
     return res.status(401).json({ error: "Unauthorized" });
   }
   try {
+    // agar ma merre token ko decode karna chahta hu toh ma usko jwt.verify() mei pass karta hu
+    // aur usko decode karta hu aur agar decode ho jata ha aur muza user mil jata ha to matlab user authenticated ha mera
     jwt.verify(token, process.env.JWT_SECRET, (error, decoded) => {
       if (error) {
         return res.status(401).json({ error: "Unauthorized" });
       } else {
-        req.user = decoded;
+        console.log("decoded", decoded);
+        req.user = decoded.username;
+        req.id = decoded.id;
+        console.log("authenticated");
         next();
       }
     });
