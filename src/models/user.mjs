ecb35d836805d84dc24ae0673d69470c1421c018
garvit <agarvit1142000@gmail.com>
2023-03-01 11:21:44 +0530

    User schema updated to add email verifiedd  and verification token

--- src/models/user.mjs
+++ src/models/user.mjs
@@ -7,9 +7,28 @@ dotenv.config();
 const saltWorkFactor = parseInt(process.env.SALT_WORK_FACTOR, 10);
 const userSchema = new mongoose.Schema(
   {
-    username: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    emailVerificationToken: {
+      type: String,
+      required: true,
+    },
+    emailVerified: {
+      type: Boolean,
+      default: false,
+    },
   },
   {
     timestamps: true,
@@ -27,9 +46,6 @@ userSchema.pre("save", async function (next) {
   } else return next();
 });
 
-const User = mongoose.model("User", userSchema);
-
-// we can crate methods on the schema and then use them on the model like this user.comparePassword
 userSchema.methods.comparePassword = async function (candidatePassword) {
   let user = this;
   try {
@@ -40,5 +56,9 @@ userSchema.methods.comparePassword = async function (candidatePassword) {
     return false;
   }
 };
+
+const User = mongoose.model("User", userSchema);
+
+// we can crate methods on the schema and then use them on the model like this user.comparePassword
 export { User };
 // moongose have hooks  pre and post hooks pre hooks are executed before an event and post hooks are executed after an event pre hooks are used before saving data to database and post hooks are used after saving data to database
