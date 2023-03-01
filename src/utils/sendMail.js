2bb40b907234c9bdb05ff8e73aea4b922004b6bf
garvit <agarvit1142000@gmail.com>
2023-03-01 11:19:23 +0530

    Function to send mail  with token

---
+++ src/utils/sendMail.js
@@ -0,0 +1,27 @@
+import nodemailer from "nodemailer";
+
+export async function sendMail(email, token) {
+  try {
+    var transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false, // use SSL/TLS,
+
+      auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD,
+      },
+    });
+
+    const mailOptions = {
+      from: process.env.EMAIL,
+      to: email,
+      subject: "Email Verification",
+      text: `localhost:5000/verify?token=${token}`,
+    };
+
+    await transporter.sendMail(mailOptions);
+  } catch (error) {
+    console.error(error);
+  }
+}
