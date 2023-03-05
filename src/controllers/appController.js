7e04e7b64ecbee98a9184c88abb1e0f80e2d38db
garvit <agarvit1142000@gmail.com>
2023-03-05 13:23:07 +0530

    handle signup functionality when any field is empty , stop send cookie in email , onboard function

--- src/controllers/appController.js
+++ src/controllers/appController.js
@@ -6,12 +6,18 @@ dotenv.config();
 
 import { sendMail } from "../utils/sendMail.js";
 
-// mana signup ka time jo email dala aur usi email sa verification token bana ka
+//mana signup ka time jo email dala aur usi email sa verification token bana ka
 //bheja usi mail pa matlab agar us mail ka link sa mera token verify ho raha ha matlab
 //decode ho raha ha iska matlab mera vo  mail valid ha
 export async function signup(req, res) {
   try {
     const { username, email, password } = req.body;
+
+    if (!username || !email || !password) {
+      return res
+        .status(400)
+        .json({ error: "Please provide username, email and password" });
+    }
     const existingUser = await User.findOne({ $or: [{ username }, { email }] });
     // write code in case we have both email and username for existing user
 
@@ -38,30 +44,24 @@ export async function signup(req, res) {
       process.env.JWT_SECRET,
       { expiresIn: "1h" }
     );
-    res.cookie("emailVerificationToken", emailVerificationToken, {
-      //  WE can aslo use query for this means send the token in the query string
-      // Its better to use link and query because we can use the link in the email and the user can click on the link and then the link will be sent to the server and then the server will verify the token so enter the token in the query string instead
-      httpOnly: false,
-      expires: new Date(Date.now() + 3600000),
-    });
+    //  res.cookie("emailVerificationToken", emailVerificationToken, {
+    //    //  WE can aslo use query for this means send the token in the query string
+    //    // Its better to use link and query because we can use the link in the email and the user can click on the link and then the link will be sent to the server and then the server will verify the token so enter the token in the query string instead
+    //    httpOnly: false,
+    //    expires: new Date(Date.now() + 3600000),
+    //  });
+    // Dont send cookie otherwise user will verify from token only  from browser itself
 
     const user = new User({
       username,
       email,
       password,
-      emailVerificationToken, // there is not much sense of saving in the database because we are sending the token in the email and then we are verifying the token in the query string
+      emailVerificationToken,
     });
 
     await user.save();
 
     await sendMail(email, emailVerificationToken);
-
-    if (User.findOne({ _id: user._id }).emailVerified !== true) {
-      User.deleteOne({ _id: user._id });
-      return res
-        .status(400)
-        .json({ error: "User not created because email not veerified" });
-    }
 
     return res.status(201).json({ message: "User created successfully" });
   } catch (error) {
@@ -127,32 +127,17 @@ export async function logout(req, res) {
   return res.status(200).send({ msg: "Logged out successfully" });
 }
 
-//export async function onboard(req, res) {
-//  try {
-//    const user = await User.findById(req.id); // we are gtting id from verifyJWT.js middleware using payload decoding from token and setting it to req.id
-//    if (!user) {
-//      return res.status(404).json({ error: "User not found" });
-//    } else {
-//      const website = req.body.website;
-//      // add website to user's websites array in database if no array then create one and no duplicates
-//      console.log(user.websites);
-//      if (user.websites) {
-//        console.log("user have wesbites");
-//        user.websites.push(website);
-//        await user.save();
-//        console.log("user website saved");
-//      }
-//      if (!user.websites) {
-//        console.log("user dont have wesbites");
-//        user.websites = [website];
-//        await user.save();
-//        console.log("user website saved");
-//      }
-//
-//      return res.status(200).json({ message: "Website added successfully" });
-//    }
-//  } catch (error) {
-//    console.error(error);
-//    return res.status(500).json({ error: "Internal server error" });
-//  }
-//}
+export async function onboard(req, res) {
+  // run this after protected middleware so that we can get the id from the token
+  try {
+    const user = await User.findById(req.id); // we are gtting id from verifyJWT.js middleware using payload decoding from token and setting it to req.id
+    if (!user) {
+      return res.status(404).json({ error: "User not found" }); // and then we are checking if that user exists or not
+    } else {
+      return res.status(200).json({ message: "User is logged in" });
+    }
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Internal server error" });
+  }
+}
