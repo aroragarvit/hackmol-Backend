031ab6ff0e806e3023489022a20ec755187ca90f
garvit <agarvit1142000@gmail.com>
2023-03-07 02:57:53 +0530

    Made some changes in socket disconnection functionality

--- src/index.js
+++ src/index.js
@@ -15,19 +15,41 @@ import { Room } from "./models/roomSchema.mjs";
 const app = express();
 const port = process.env.PORT;
 const server = createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["my-custom-header"],
+    credentials: true,
+  },
+});
 
 // Handle socket.io connections
+
+// Middleware for CORS and headers
+app.use(cookieParser());
+app.use(cors());
+app.use(express.json());
+app.use(morgan("tiny"));
+app.disable("x-powered-by");
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Credentials", true);
+  next();
+});
+
 io.on("connection", async (socket) => {
   // Use socket authentication middleware to verify token
+  console.log("Socket use effect going to run");
   io.use(async (socket, next) => {
-    const token = socket.handshake.auth.token;
+    const token = socket.handshake.query.token;
     if (!token) {
-      return next(new Error("Authentication error"));
+      return next(new Error("Authentication error - no token provided"));
     }
     try {
       const decoded = await jwt.verify(token, process.env.JWT_SECRET);
       socket.decoded = decoded;
+      console.log("Authenticated");
       next();
     } catch (error) {
       return next(new Error("Authentication error"));
@@ -38,6 +60,7 @@ io.on("connection", async (socket) => {
   socket.on("join room", async (data) => {
     console.log("New client connected");
     const { id } = socket.client;
+    console.log("id", id);
     const interests = data.interests;
     const rooms = await Room.find({ interests: { $in: interests } });
     if (rooms.length) {
@@ -47,8 +70,13 @@ io.on("connection", async (socket) => {
         io.to(room._id).emit("user joined", { userId: id });
       } else {
         const newRoom = await Room.create({ interests, users: [id] });
+        console.log("newRoom");
         socket.join(newRoom._id);
       }
+    } else {
+      const newRoom = await Room.create({ interests, users: [id] });
+      console.log("newRoom");
+      socket.join(newRoom._id);
     }
   });
 
@@ -59,21 +87,21 @@ io.on("connection", async (socket) => {
   });
 
   // Handle "disconnect" event
-  socket.on("disconnect", () => {
-    console.log("Client disconnected");
+  socket.on("disconnect", async () => {
+    console.log(`Client ${id} disconnected`);
+    const room = await Room.findOne({ users: id });
+    if (room) {
+      // Remove the user from the room's users list
+      room.users = room.users.filter((userId) => userId !== id);
+      await room.save();
+      console.log(`Removed client ${id} from room ${room._id}`);
+      // If there's only one user left in the room, delete the room
+      if (room.users.length === 0) {
+        await Room.findByIdAndDelete(room._id);
+        console.log(`Deleted room ${room._id}`);
+      }
+    }
   });
-});
-
-// Middleware for CORS and headers
-app.use(cookieParser());
-app.use(cors());
-app.use(express.json());
-app.use(morgan("tiny"));
-app.disable("x-powered-by");
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Credentials", true);
-  next();
 });
 
 // Routes
@@ -83,7 +111,6 @@ app.get("/", (req, res) => {
 app.post("/signup", signup);
 app.post("/login", login);
 app.get("/protected", verifyToken, (req, res) => {
-  console.log("hello");
   res.end();
 });
 app.get("/verify", verifyEmail);
